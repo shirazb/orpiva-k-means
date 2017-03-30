aa0d7497b15cc87ef7bb8ae1f01ee5562b887215
Shiraz Butt <sb4515@ic.ac.uk>
2017-03-30 22:56:08 +0100

    Made data types for AST

---
+++ src/ARFFParser/AST.hs
@@ -0,0 +1,42 @@
+{-
+  The AST of an ARFF file.
+
+  The context free grammar is taken from
+      https://list.waikato.ac.nz/pipermail/wekalist/2008-January/038545.html
+-}
+
+module ARFFParser.AST where
+
+data ARFF
+  = ARFF Header Data
+data Header
+  = Header String [Adecl] -- cannot be empty
+
+data Adecl
+  = Adecl String DataType
+
+data DataType
+  = Numeric
+  | Integer
+  | Real
+  | String
+  | Relational [Adecl] String -- cannot be empty
+  | DateData Date
+  | Values [Value] -- cannot be empty
+
+data Date
+  = Date (Maybe String)
+
+data Data
+  = PairData [Pair] -- cannot be empty
+  | ValueData [Value] -- cannot be empty
+
+data Pair
+  = Pair Int Value
+
+-- Ensure only valid floats, ints, strings parsed
+data Value
+  = QMark
+  | FloatVal Float
+  | IntVal Int
+  | StringVal String
